b7efd82b99e5e073573adf9f10af0db76e6d2864
momo1108 <momo1108@daum.net>
2023-12-05 18:05:30 +0900

    test ssh-add

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const {parse} = require("marked");
 
 /**
- * 원본 html, 스캔용 html 분리해서 사용?
- * 객체 생성)
- * h1 스캔 -> 발견 시 처음 끝 체크 -> 원본 위치 기억, h2 스캔용 html 생성. 
- * 위 스캔용 html에서 h2 스캔 -> 발견 시 처음 끝 체크 -> h2 스캔용 html 원본 위치 기억, h3 스캔용 html 생성
- * 위 스캔용 html에서 h3 스캔 -> 발견 시 처음 끝 체크 -> h3 스캔용 html 원본 위치 기억, h4 스캔용 html 생성
- * 위 스캔용 html에서 h4 스캔 -> 발견 시 처음 끝 체크 -> h4 스캔용 html 원본 위치 기억, h5 스캔용 html 생성
- * 위 스캔용 html에서 h5 스캔 -> 발견 시 처음 끝 체크 -> h5 스캔용 html 원본 위치 기억, h6 스캔용 html 생성
- * 위 스캔용 html에서 h6 스캔 -> 발견 시 처음 끝 체크 -> h6 스캔용 html 원본 위치 기억
+ * findHeader 메서드 재귀호출
  */
 class _MarkdownHeaderStructure {
     #headerInfo = [];
