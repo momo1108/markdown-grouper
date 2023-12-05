94910ad7bedaf494348360bc1894ef8c2cae078a
momo1108 <momo1108@daum.net>
2023-12-05 18:00:46 +0900

    update src

--- app.js
+++ app.js
@@ -11,26 +11,71 @@ const {parse} = require("marked");
  * 위 스캔용 html에서 h6 스캔 -> 발견 시 처음 끝 체크 -> h6 스캔용 html 원본 위치 기억
  */
 class _MarkdownHeaderStructure {
-    #h1 = 0;
-    #h2 = 0;
-    #h3 = 0;
-    #h4 = 0;
-    #h5 = 0;
-    #h6 = 0;
+    #headerInfo = [];
 
     constructor(html) {
-        const result = this.findHeader(html, 1);
+        const result = this.findHeader(html, 1, 0, "");
         console.log(result);
     }
 
     // start는 레벨에 맞는 모든 헤더태그 찾기
     // end는 해당 레벨 이상의 모든 태그 찾기
     // 함수 내부에서 현재 레벨에 해당하는 모든 태그를 체크해야함. while문 사용하고 반복시마다 이전태그 다음으로 index 이동
-    findHeader(text, level, index) {
-        const keyword = `<h${level}>`
-        const start = text.indexOf(keyword, index);
-        const end = text.indexOf(keyword, index);
-        return [start, end];
+    findHeader(text, level, order, prevLabel) {
+        if(level > 6) return text;
+
+        const startPattern =  new RegExp(`<h${level}>.+</h${level}>`, "g")
+        const endPattern = new RegExp(`<h[${Array.from(Array(level), (_, index)=>index+1).join("")}]>`, "g");
+
+        let start = startPattern.exec(text);
+
+        // 처음 해당 레벨이 존재하는가?
+        if(start){
+            let endCheck = endPattern.exec(text.slice(start.index+4));
+            let end = endCheck? endCheck.index : text.length;
+        
+            let textBeforeStart, textFromStartToEnd, textAfterEnd;
+        
+            while(start) {
+                if(order===0){
+                    textBeforeStart = text.slice(0, start.index);
+                    textFromStartToEnd = text.slice(start.index, end);
+                    textAfterEnd = text.slice(end);
+                } else {
+                    textBeforeStart = textAfterEnd.slice(0, start.index);
+                    textFromStartToEnd = textAfterEnd.slice(start.index, end);
+                    textAfterEnd = textAfterEnd.slice(end);
+                }
+        
+                const curLabel = `${prevLabel}_h${level}-${order}`;
+                const content = findHeader(textFromStartToEnd, level+1, 0, curLabel);
+        
+                if(order===0){
+                    text = `${textBeforeStart}
+<section id="${curLabel}">
+${content}
+</section>`
+                } else {
+                    text = `${text}
+${textBeforeStart}
+<section id="${curLabel}">
+${content}
+</section>`
+                }
+        
+                order++;
+                start = startPattern.exec(textAfterEnd);
+                endCheck = endPattern.exec(textAfterEnd.slice(start+4));
+                end = endCheck? endCheck.index : text.length;
+            }
+        
+            text = `${text}
+${textAfterEnd}`;
+        } else {
+            text = findHeader(text, level+1, 0, "");
+        }
+
+        return text;
     }
 }
 
